297576f11f87c227d2a82b2ae87990e2be8e0c1e
tlear38 <qodlgks123@naver.com>
2022-03-01 18:16:51 +0900

    commit

---
+++ pybo/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from pybo.models import Question, Answer
+
+
+class QuestionForm(forms.ModelForm):
+    class Meta:
+        model = Question
+        fields = ['subject', 'content']
+        # 제목 내용 한글 변경
+        labels = {
+            'subject': '제목',
+            'content': '내용',
+        }
+
+
+class AnswerForm(forms.ModelForm):
+    class Meta:
+        model = Answer
+        fields = ['content']
+        labels = {
+            'content': '답변 내용',
+        }
