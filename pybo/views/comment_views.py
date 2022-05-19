7fca759125fb52cbea87a7fb4f86a35cf4935709
teior <qodlgks123@naver.com>
2022-05-19 23:30:10 +0900

    urls.py 파일 구조 변경

--- pybo/views/comment_views.py
+++ pybo/views/comment_views.py
@@ -4,7 +4,25 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 
 from ..forms import CommentForm
-from ..models import Answer, Comment
+from ..models import Answer, Comment, Question
+
+
+@login_required(login_url="common:login")
+def comment_create_question(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.author = request.user
+            comment.create_date = timezone.now()
+            comment.question = question
+            comment.save()
+            return redirect('pybo:detail', question_id=question.id)
+    else:
+        form = CommentForm()
+    context = {'form': form}
+    return render(request, 'pybo/comment_form.html', context)
 
 
 @login_required(login_url="common:login")
